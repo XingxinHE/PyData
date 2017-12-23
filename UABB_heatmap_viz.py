aa36c089e1cd2bfd88a0321f664e9acbce6c0f1f
Xingxin Morris He <30434598+XingxinMorris@users.noreply.github.com>
2017-12-23 15:47:45 +0800

    add while loop

--- UABB_heatmap_viz.py
+++ UABB_heatmap_viz.py
@@ -1,35 +1,46 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#set the path
-file_path = r'E:\xkoolWorkshop\workshop\people_stream_data\2017121419.tsv'
-
-#read the file
-data = pd.read_csv(file_path, sep = '\t')
-
-#subset the data and delete useless info
-str_data  = pd.DataFrame(data, columns = ['lng', 'lat', 'val'])
-
 #import seaborn
 import seaborn as sns
-sns.set()
+
+day_int = 20
+time_int = 0
 
 
-#construct a new data frame, from long data to matrix
-str_new_data = str_data.pivot("lat", "lng", "val")
+while time_int <= 9:
+    #set the path
+    day = str(day_int)
+    time = str(time_int)
+    fileType = '.tsv'
+    file_pre_path = '201712'
+    file_path = file_pre_path+day+'0'+time+fileType
+    
+    #read the file
+    data = pd.read_csv(file_path, sep = '\t')
 
-#set the cell size
-f, ax = plt.subplots(figsize=(9, 6))
+    #subset the data and delete useless info
+    str_data  = pd.DataFrame(data, columns = ['lng', 'lat', 'val'])
 
-#plot it
-this_heatmap = sns.heatmap(str_new_data)
-final = this_heatmap.invert_yaxis()
 
-from pylab import savefig
-savefig('111.png')
+    sns.set()
 
-print('done')
 
-lng_list = data['lng']
-dif_lng_num = len( set(lng_list) )
+    #construct a new data frame, from long data to matrix
+    str_new_data = str_data.pivot("lat", "lng", "val")
+
+    #set the cell size
+    f, ax = plt.subplots(figsize=(9, 6))
+    f.set_size_inches(16.5, 11.7)
+
+    #plot it
+    this_heatmap = sns.heatmap(str_new_data)
+    final = this_heatmap.invert_yaxis()
+
+    from pylab import savefig
+
+    png_path = '12'+day+'0'+time+'.png'
+
+    savefig(png_path, dpi = 300)
+    
+    time_int += 1
